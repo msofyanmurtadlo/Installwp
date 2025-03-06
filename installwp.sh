c6829ad897cae73edbb9a2eb0132a61b2fb07abb
msofyanmurtadlo <sofyanmurtadlo6@gmail.com>
2025-03-06 20:29:28 +0700

    fix

--- installwp.sh
+++ installwp.sh
@@ -91,7 +91,7 @@ sudo add-apt-repository ppa:ondrej/php -y &>/dev/null
 sudo apt update -y &>/dev/null
 
 # Install PHP 8.4 and required extensions in one command
-install_with_progress php8.4-fpm php8.4-common php8.4-dom php8.4-intl php8.4-mysql php8.4-xml php8.4-xmlrpc php8.4-curl php8.4-gd php8.4-imagick php8.4-cli php8.4-dev php8.4-imap php8.4-mbstring php8.4-soap php8.4-zip php8.4-bcmath -y
+install_with_progress php8.4-fpm php8.4-common php8.4-dom php8.4-intl php8.4-mysql php8.4-xml php8.4-xmlrpc php8.4-curl php8.4-gd php8.4-imagick php8.4-cli php8.4-dev php8.4-imap php8.4-mbstring php8.4-soap php8.4-zip php8.4-bcmath 
 
 # Prompt for domain and database details
 echo -e "\n\e[1;34mEnter your domain (e.g. example.com):\e[0m"
