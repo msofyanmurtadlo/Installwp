54124d4ebdf26c092ee70917ab8d58a54085511e
Sofyan <sofyanmurtadlo6@gmail.com>
2025-09-20 02:03:53 +0700

    final & fix

--- ubuntu24.sh
+++ ubuntu24.sh
@@ -31,7 +31,8 @@ run_task() {
     return 0
   else
     echo -e "${C_RED}[GAGAL]${C_RESET}"
-    log "error" "Gagal menjalankan '$description'. Periksa log sistem untuk detail."
+    log "warn" "Gagal menjalankan '$description'. Ini mungkin tidak signifikan jika sudah dikonfigurasi sebelumnya."
+    return 1
   fi
 }
 
@@ -60,6 +61,7 @@ setup_server() {
   log "info" "Mengamankan instalasi MariaDB..."
   run_task "Mengaktifkan & memulai MariaDB" systemctl enable --now mariadb.service
   
+  # Perubahan: Mengubah run_task dari error menjadi warn
   run_task "Menghapus database tes dan user anonim" mysql -e "DROP DATABASE IF EXISTS test; DELETE FROM mysql.user WHERE User=''; FLUSH PRIVILEGES;"
 
   if [ -s "$password_file" ]; then
@@ -205,7 +207,7 @@ EOF
     if [ $? -eq 0 ]; then
       log "success" "File SSL DH params berhasil dibuat!"
     else
-      log "error" "Gagal membuat file SSL DH params. Periksa log atau jalankan manual."
+      log "warn" "Gagal membuat file SSL DH params. Ini mungkin tidak signifikan."
     fi
   else
     log "info" "File SSL DH params sudah ada. Melewati pembuatan."
